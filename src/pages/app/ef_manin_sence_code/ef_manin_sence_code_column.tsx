0a8b66723db4a06046e82cfb9b7f267746ebcdda
Tong Li <litongjava001@gmail.com>
2025-05-02 09:20:49 -1000

    init commit

---
+++ src/pages/app/ef_manin_sence_code/ef_manin_sence_code_column.tsx
@@ -0,0 +1,73 @@
+import {ProColumns} from '@ant-design/pro-components';
+
+export const ef_manin_sence_code_columns = (): ProColumns<any>[] => [
+  {
+    "title": "topic",
+    "dataIndex": "topic",
+    "valueType": "text"
+  },
+  {
+    "title": "sence",
+    "dataIndex": "sence",
+    "valueType": "text"
+  },
+  {
+    "title": "loop",
+    "dataIndex": "loop",
+    "valueType": "text"
+  },
+  {
+    "title": "code",
+    "dataIndex": "code",
+    "valueType": "textarea",
+    ellipsis:true,
+    copyable: true,
+  },
+  {
+    "title": "std_out",
+    "dataIndex": "std_out",
+    "valueType": "textarea",
+    ellipsis:true,
+    copyable: true,
+  },
+  {
+    "title": "std_err",
+    "dataIndex": "std_err",
+    "valueType": "textarea",
+    ellipsis:true,
+    copyable: true,
+
+  },
+  {
+    "title": "uri",
+    "dataIndex": "uri",
+    "valueType": "text",
+    copyable: true,
+  },
+  {
+    "title": "status",
+    "dataIndex": "status",
+    "valueType": "text"
+  },
+  {
+    "title": "elapsed",
+    "dataIndex": "elapsed",
+    "valueType": "text"
+  },
+  {
+    title: "update_time",
+    dataIndex: "update_time",
+    valueType: "dateTime",
+    hideInSearch: true,
+    hideInForm: true,
+  },
+  {
+    key: "update_time",
+    title: "update_time",
+    dataIndex: "update_time_range",
+    valueType: "dateTimeRange",
+    hideInTable: true,
+    hideInForm: true,
+    hideInDescriptions: true,
+  },
+];
---
+++ src/pages/app/ef_manin_sence_code/ef_manin_sence_code_index.tsx
@@ -0,0 +1,36 @@
+import ApiTableLong from '@/components/common/ApiTableLong';
+import { ef_manin_sence_code_columns } from '@/pages/app/ef_manin_sence_code/ef_manin_sence_code_column';
+
+export default () => {
+  const from = 'ef_manin_sence_code';
+  const beforePageRequest = (params: any, isRecoveryMode?: boolean) => {
+    params.idType = 'long';
+
+    if (isRecoveryMode) {
+      params.deleted = 1;
+    } else {
+      params.deleted = 0;
+    }
+    params.orderBy = 'update_time';
+    params.isAsc = 'false';
+    params.update_time_type = 'string[]';
+    params.update_time_op = 'bt';
+    params.topicOp = 'ct';
+    return params;
+  };
+
+  const beforeCreateRequest = (formValues: any) => {
+    return {
+      ...formValues,
+      idType: 'long',
+    };
+  };
+  return (
+    <ApiTableLong
+      from={from}
+      columns={ef_manin_sence_code_columns()}
+      beforePageRequest={beforePageRequest}
+      beforeCreateRequest={beforeCreateRequest}
+    />
+  );
+};
